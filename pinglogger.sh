84ccef2d454d09beee122b9f5d5bd81fa0ab4de8
vincent <vmmurph@gmail.com>
2025-09-17 09:40:10 -0600

    fixed disconnect print; changed timestamp

--- pinglogger.sh
+++ pinglogger.sh
@@ -3,26 +3,23 @@ TARGET="$1"
 INTERVAL=5
 LINE=""
 LAST_HOUR=""
-
+START_TS=$(date "+%Y-%m-%d %I:%M%p")
+echo "$START_TS:"
 while true; do
-    TIMESTAMP=$(date "+%Y-%m-%d %I%p")
+    TIMESTAMP=$(date "+%Y-%m-%d %I:%M%p")
     HOUR=$(date "+%Y-%m-%d %I%p")
-
     if [[ "$HOUR" != "$LAST_HOUR" && -n "$LINE" ]]; then
         # Print previous hour's line, reset for new hour
         echo "$LAST_HOUR:"
         echo "$LINE"
         LINE=""
     fi
-
     ping -c 1 -W 2 "$TARGET" > /dev/null 2>&1
     if [[ $? -eq 0 ]]; then
         printf "."; LINE="${LINE}."
-
     else
-        printf "."; LINE="${LINE}x"
+        printf "x"; LINE="${LINE}x"
     fi
-
     LAST_HOUR="$HOUR"
     sleep $INTERVAL
 done
